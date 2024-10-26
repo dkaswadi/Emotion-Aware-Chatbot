6e387744ae43b5e86e99a90a2d0fb180dd83eae0
dkaswadi <dkaswadi@gmail.com>
2024-10-26 16:17:30 +0800

    Update text_to_speech.py

--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -4,7 +4,7 @@ def speak_response(text):
     engine = pyttsx3.init()
     
     # Set the desired speaking rate (default is around 200, increase for faster speech)
-    engine.setProperty('rate', 250)  # Adjust this value to control speech speed
+    engine.setProperty('rate', 200)  # Adjust this value to control speech speed
 
     engine.say(text)
     engine.runAndWait()
