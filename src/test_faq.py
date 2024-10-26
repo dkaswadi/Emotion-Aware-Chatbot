31228d0d4ee5aea32b1921aa4d05ff0c1428ed5e
dkaswadi <dkaswadi@gmail.com>
2024-10-26 15:45:22 +0800

    Delete src/test_faq.py

--- src/test_faq.py
+++
@@ -1,9 +0,0 @@
-from faq_module import find_answer
-
-while True:
-    user_question = input("Ask a question: ")
-    if user_question.lower() in ["exit", "quit"]:
-        break
-    response = find_answer(user_question)
-    print(f"Chatbot answer: {response}")
-
