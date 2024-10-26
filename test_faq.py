3e6a35b7e4ac537141b0d704bc5339943fcdbcfd
dkaswadi <dkaswadi@gmail.com>
2024-10-26 15:45:49 +0800

    Update test_faq.py

--- test_faq.py
+++ test_faq.py
@@ -1,1 +1,9 @@
+from faq_module import find_answer
+
+while True:
+    user_question = input("Ask a question: ")
+    if user_question.lower() in ["exit", "quit"]:
+        break
+    response = find_answer(user_question)
+    print(f"Chatbot answer: {response}")
 
