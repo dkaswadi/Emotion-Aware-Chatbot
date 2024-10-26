abe6aced99e1ca67127fb7b04a150a919b142e7b
dkaswadi <dkaswadi@gmail.com>
2024-10-26 15:47:45 +0800

    Update test_faq.py

--- test_faq.py
+++ test_faq.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+# Adding the `src` directory to the system path for module imports
+current_dir = os.path.dirname(os.path.abspath(__file__))
+src_path = os.path.join(current_dir, 'src')
+sys.path.append(src_path)
+
 from faq_module import find_answer
 
 while True:
@@ -6,4 +14,3 @@ while True:
         break
     response = find_answer(user_question)
     print(f"Chatbot answer: {response}")
-
