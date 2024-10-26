cfb6323d607c081e77e6e916b5bc4cf134f8586d
dkaswadi <dkaswadi@gmail.com>
2024-10-26 13:20:55 +0800

    Update voice_to_text.py

--- src/voice_to_text.py
+++ src/voice_to_text.py
@@ -1,1 +1,17 @@
+import speech_recognition as sr
+
+def listen_to_user():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("I'm listening...")
+        audio = recognizer.listen(source)
+        
+        try:
+            text = recognizer.recognize_google(audio)
+            print(f"You said: {text}")
+            return text
+        except sr.UnknownValueError:
+            return "Sorry, I didn't catch that."
+        except sr.RequestError:
+            return "Sorry, there seems to be a problem with the service."
 
